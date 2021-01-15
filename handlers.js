04e4b7845a12a78bd3684326f8e29a4646f32a98
Kat Sauma <katsauma@gmail.com>
2021-01-14 17:56:38 -0800

    start refactor, tried to clean up app.js code

---
+++ handlers.js
@@ -0,0 +1,42 @@
+// import { countsAsAYes } from './utils.js';
+
+// export function takeQuiz() {
+//     alert('Prepare yourself Summoner!');
+
+//     const confirmation = confirm('Do you wish to proceed?');
+
+
+//     if (!confirmation) {
+//         return;
+//     }
+
+//     const firstName = prompt('Choose your Summoner Name! (First Name)');
+//     const lastName = prompt('Choose your Summoner Name! (Last Name)');
+
+//     const firstAnswer = prompt('Was League of Legends inspired by Starcraft?');
+//     let correctAnswers = 0;
+//     if (countsAsAYes(firstAnswer)) {
+
+//     }
+//     else {
+//         correctAnswers++;
+//     }
+
+//     const secondAnswer = prompt('Do you win by destroying your enemys nexus?');
+//     if (countsAsAYes(secondAnswer)) {
+//         correctAnswers++;
+
+//     }
+//     else {
+
+//     }
+
+//     const thirdAnswer = prompt('Did the game regularly peak at 8 million concurrent players in 2019?');
+//     if (countsAsAYes(thirdAnswer)) {
+//         correctAnswers++;
+
+//     }
+//     else {
+
+//     }
+// }
